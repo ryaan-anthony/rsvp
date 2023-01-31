8f3dbe651952fc2f52ea973fdd0fa5b483ea913f
ryaan-anthony <rtulino@gmail.com>
2023-01-31 18:39:20 -0500

    login from qr code

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/', to: 'sites#show', as: 'sites'
   get 'login', to: 'login#index'
   post 'login', to: 'login#create'
+  get 'qr', to: 'login#create'
   post 'guests', to: 'guests#update'
   post 'rsvp', to: 'guests#rsvp'
 end
